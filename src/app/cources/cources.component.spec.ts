9020966169b973506d516451d873bb126b2e9bb3
alexm <alexmsystems@gmail.com>
2016-12-21 15:07:11 +0300

    fix1

---
+++ src/app/cources/cources.component.spec.ts
@@ -0,0 +1,35 @@
+import { ActivatedRoute, Data } from '@angular/router';
+import { Component } from '@angular/core';
+import { inject, TestBed } from '@angular/core/testing';
+
+// Load the implementations that should be tested
+import { CourcesComponent } from './cources.component';
+
+describe('Cources', () => {
+  // provide our implementations or mocks to the dependency injector
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      // provide a better mock
+      {
+        provide: ActivatedRoute,
+        useValue: {
+          data: {
+            subscribe: (fn: (value: Data) => void) => fn({
+              yourData: 'yolo'
+            })
+          }
+        }
+      },
+      CourcesComponent
+    ]
+  }));
+
+  it('should log ngOnInit', inject([CourcesComponent], (about: CourcesComponent) => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    about.ngOnInit();
+    expect(console.log).toHaveBeenCalled();
+  }));
+
+});
